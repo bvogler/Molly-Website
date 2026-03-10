ef3afd96ebb495f0f9d50a3e69e6b1d30a5ed0b3
bvogler <brian@vogler.com>
2026-03-10 09:18:51 -0400

    Added siteConfig.js with version number, updated index.html to use siteConfig.js and display version number in footer, and made minor adjustments to the version display in index.html.

---
+++ siteConfig.js
@@ -0,0 +1,3 @@
+const SITE_NAME = "Molly Loch";
+const SITE_VERSION = "1.015";
+const ADMIN_LOGIN_URL = "https://bkvogler.com/molly_admin/index.html";
--- xconfig.js
+++ xconfig.js
@@ -1,4 +1,5 @@
 // config.js
-const APP_VERSION = "1.0.15";
+const SITE_NAME = "Molly Loch";
 const SITE_VERSION = "1.012";
+const APP_VERSION = "1.0.15x";
 const ADMIN_LOGIN_URL = "https://bkvogler.com/molly_admin/index.html";
